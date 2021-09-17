c3f9dc44430883d7e251b2d4f3d1daae18aacc56
GlebOlegovich <godleib@yandex.ru>
2021-09-17 23:48:19 +0300

    1ый посыл

---
+++ app_logger.py
@@ -0,0 +1,39 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+_log_format = (
+    "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s)."
+    "%(funcName)s(%(lineno)d) - %(message)s"
+)
+
+
+def get_file_handler():
+    file_handler = RotatingFileHandler(
+        'my_logger.log',
+        maxBytes=10000,
+        backupCount=5
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+# В ТЗ говорится про StreamHandler(), им стримить в терминал можно итп,
+# но еще нужно стримить errors в ТГ, как я понял -
+# это надо сделать StreamHandler()ом, но я хз как)
+# Поэтому я пока что сделал просто
+# вызов функции отправки сообщения, мб можешь подсказать,
+# как мне сделать это через стрим?
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.ERROR)
+    stream_handler.setFormatter(logging.Formatter(_log_format))
+    return stream_handler
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_file_handler())
+    logger.addHandler(get_stream_handler())
+    return logger
--- homework.py
+++ homework.py
@@ -1,8 +1,16 @@
+import logging
 import os
 import time
+from http import HTTPStatus
+from typing import Dict
+
 import requests
 import telegram
 from dotenv import load_dotenv
+
+import app_logger
+
+# from pprint import pprint
 
 
 load_dotenv()
@@ -10,43 +18,121 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+YA_PRACTICUM_HW_URL = (
+    'https://praktikum.yandex.ru/api/'
+    'user_api/homework_statuses/'
+)
+
+STICKERS = {
+    'понравилось': 'CAACAgIAAxkBAAEC6zthRGKOmkZuxSASJlwj'
+                   's24h7cMh-QACNQADrWW8FPWlcVzFMOXgIAQ',
+
+    'ошибки': 'CAACAgIAAxkBAAEC6lRhQ6i2KM00tMRK-c'
+              '_5t3B-nm7xTAACeQgAAgi3GQLo6uffKEXZAAEgBA',
+
+    'проверку': 'CAACAgIAAxkBAAEC60JhRGNgDpeRmgx3SWX-O_'
+                'KsDPA_2QACTgADwDZPE5XvE_fE3wn3IAQ',
+
+    'упал': 'CAACAgIAAxkBAAEC60xhRGkcD0O7P_Fu11'
+            'UWeXOemidxMQACPgADspiaDv-4iTC3LFg0IAQ',
+}
 
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
-def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+def parse_homework_status(homework: Dict) -> str:
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
+    elif homework['status'] == 'approved':
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    elif homework['status'] == 'reviewing':
+        return (
+            f'Работу "{homework["lesson_name"]} - {homework_name}"'
+            f' взяли на проверку! ({homework["date_updated"]})'
+        )
+    else:
+        message = 'Нам пришел json, со странным значением статуса : '
+        message += f'{homework["status"]} в дз : {homework["homework_name"]} '
+        raise ValueError(message)
+    return (
+        f'У вас проверили работу "{homework["lesson_name"]} - {homework_name}"'
+        f'\nВремя: {homework["date_updated"]}!\n{verdict}'
+    )
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
-    return homework_statuses.json()
+    homework_statuses = requests.get(
+        YA_PRACTICUM_HW_URL,
+        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
+        params={'from_date': current_timestamp}
+    )
+    if homework_statuses.status_code != HTTPStatus.OK:
+        raise ValueError(
+            f'Сервер отвалился - {homework_statuses.status_code}'
+            f', при переходе по {YA_PRACTICUM_HW_URL}'
+        )
+    else:
+        return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    for key, sticker in STICKERS.items():
+        if message.find(key) != -1:
+            bot.send_sticker(
+                chat_id=CHAT_ID,
+                sticker=sticker
+            )
+            break
+
+    logger.info(f'Бот отпрвил сообщение: {message}')
+    return bot.send_message(
+        chat_id=CHAT_ID,
+        text=message
+    )
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    # current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = 1626455426
+    logger.debug('Бот запущен!')
 
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            hw_statuses_json = (get_homeworks(current_timestamp))
+            current_timestamp = hw_statuses_json['current_date']
 
-        except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
+            #  Если что то есть в этом списке...
+            if hw_statuses_json['homeworks']:
+                logger.info(
+                    f'Пришли проверенные ДЗ: '
+                    f'{len(hw_statuses_json["homeworks"])} шт.'
+                )
+                # pprint(hw_statuses_json)
+                for homework in hw_statuses_json['homeworks']:
+                    verdict = parse_homework_status(homework)
+                    send_message(verdict)
+
+            time.sleep(13 * 60)  # Опрашивать раз в пять минут
+
+        except Exception as error:
+            message = (
+                'Бот упал (ну вообще - не упал, а просто ошибка) '
+                f'с ошибкой: {error}'
+            )
+            logger.error(error)
+            send_message(message)
+            time.sleep(60)
 
 
 if __name__ == '__main__':
+    logger = app_logger.get_logger(__name__)
+
+    # Костыль) я сделал логер отдельным файлом, а пайтесты ругались,
+    # пока логера не было тут
+    logging
+
     main()
